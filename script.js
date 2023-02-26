01660b61260270a6370faeeee24d68fc1dd4adee
suneel yekasiri <119652179+suneel927@users.noreply.github.com>
2023-02-26 16:35:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+const registrationForm = document.querySelector('#registration-form');
+const submittedFirstName = document.querySelector('#submitted-first-name');
+const submittedLastName = document.querySelector('#submitted-last-name');
+const submittedName=document.querySelector('#submitted-name');
+const submittedDateOfBirth = document.querySelector('#submitted-date-of-birth');
+const submittedCurrentAddress = document.querySelector('#submitted-current-address');
+const submittedEmail = document.querySelector('#submitted-email');
+const submittedGender = document.querySelector('#submitted-gender');
+const submittedPhoneNumber = document.querySelector('#submitted-phone-number');
+const submittedImage = document.querySelector('#submitted-image');
 
+registrationForm.addEventListener('submit', (e) => {
+e.preventDefault();
+submittedFirstName.innerText = registrationForm['first-name'].value;
+submittedLastName.innerText = registrationForm['last-name'].value;
+submittedName.innerText=submittedFirstName.innerText+" "+submittedLastName.innerText;
+submittedDateOfBirth.innerText = registrationForm['date-of-birth'].value;
+submittedCurrentAddress.innerText = registrationForm['current-address'].value;
+submittedEmail.innerText = registrationForm['email'].value;
+submittedGender.innerText = registrationForm['gender'].value;
+submittedPhoneNumber.innerText = registrationForm['phone-number'].value;
+// Display uploaded image
+if (registrationForm['upload-picture'].files.length > 0) {
+	const uploadedImage = registrationForm['upload-picture'].files[0];
+	const imageReader = new FileReader();
+	imageReader.readAsDataURL(uploadedImage);
+	imageReader.onload = (e) => {
+		submittedImage.innerHTML = `<img src="${e.target.result}" alt="Uploaded Picture">`;
+	};
+} else {
+	submittedImage.innerHTML = '';
+}
+
+// Clear form
+registrationForm.reset();
+});
